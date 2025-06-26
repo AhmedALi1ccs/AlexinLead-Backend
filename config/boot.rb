c8dff5ef89f4b25bf7fa1e700d6bd69650999376
Ahmedali270402 <ahmed@goccs.net>
2025-06-26 22:58:59 +0300

    b

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-require_relative "config/preload"
+require_relative "preload"
 
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
